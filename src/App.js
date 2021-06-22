1e0c5071307610a5d64f173c94294538ee2967e0
techswarn <tech.swarns@gmail.com>
2021-06-22 21:09:28 +0530

    From completed with react context and usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import BookContextProvider from "./Context/BookContext";
 import NavBar from "./Components/Navbar";
-
+import BookList from "./Components/BookList";
+import BookForm from "./Components/BookFrom";
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <NavBar />
+        <BookList />
+        <BookForm />
       </BookContextProvider>
     </div>
   );
